b5548a464dd1de1e14d2c283b55b81a4b99119bd
설연수 <seolnavy@naver.com>
2021-11-20 20:07:38 +0900

    사장님,배송기사 구현

--- fastcampus/urls.py
+++ fastcampus/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('order/', include('order.urls')),
+    path('boss/', include('boss.urls')),
+    path('delivery/', include('delivery.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
